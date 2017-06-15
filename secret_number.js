36324168b2cdb017b69762b93550edd73dd59b26
John Atkins <johnatkins1999@yahoo.com>
2017-06-15 11:58:15 -1000

    init structure

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+
+  let secretNumber = (function(){
+    let randomNumber = 0;
+    return function(){
+      randomNumber = Math.floor(Math.random()*1000001);
+      return randomNumber;
+    }
+  })();
+
 };
